5b7ae53e989644cfce6b2172ea82505d966dc0bc
Cameron Dawson <cdawson@mozilla.com>
2011-09-23 16:04:00 -0700

    add GPL license to source files

--- static/fonts/cambridgeround_italic_macroman/specimen_files/easytabs.js
+++ static/fonts/cambridgeround_italic_macroman/specimen_files/easytabs.js
@@ -1,3 +1,23 @@
+/*
+Case Conductor is a Test Case Management system.
+Copyright (C) 2011 uTest Inc.
+
+This file is part of Case Conductor.
+
+Case Conductor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Case Conductor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Case Conductor.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 (function($){$.fn.easyTabs=function(option){var param=jQuery.extend({fadeSpeed:"fast",defaultContent:1,activeClass:'active'},option);$(this).each(function(){var thisId="#"+this.id;if(param.defaultContent==''){param.defaultContent=1;}
 if(typeof param.defaultContent=="number")
 {var defaultTab=$(thisId+" .tabs li:eq("+(param.defaultContent-1)+") a").attr('href').substr(1);}else{var defaultTab=param.defaultContent;}
